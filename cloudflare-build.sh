6be0c190290ea4aa73ac3d49072af159f2c5cd79
Your Name <you@example.com>
2025-11-18 21:32:38 -0500

    Add more debugging to build script

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+# Print debug information
+echo "Starting custom build script..."
+echo "NPM_AUTH_TOKEN is ${NPM_AUTH_TOKEN:0:3}...${NPM_AUTH_TOKEN: -3}"
+
 # Create a .npmrc file with the auth token
 echo "@neurodivers:registry=https://npm.pkg.github.com" > .npmrc
 echo "//npm.pkg.github.com/:_authToken=${NPM_AUTH_TOKEN}" >> .npmrc
 
+echo "Created .npmrc file:"
+cat .npmrc
+
 # Install dependencies
+echo "Installing dependencies..."
 npm install
 
 # Disable treating warnings as errors
 export CI=false
 
 # Build the project
+echo "Building project..."
 npm run build
